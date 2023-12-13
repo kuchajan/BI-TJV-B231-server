146f1cd3a8898f5d748fa4f63f3963fbd724914d
Jan Kuchař <kuchaj19@fit.cvut.cz>
2023-12-13 10:11:24 +0100

    Initialize project

---
+++ src/main/java/cz/cvut/fit/tjv/kuchaj19/carleaseapi/CarLeaseApi.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.tjv.kuchaj19.carleaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarLeaseApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarLeaseApi.class, args);
+	}
+
+}
---
+++ src/test/java/cz/cvut/fit/tjv/kuchaj19/carleaseapi/CarLeaseApiTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.tjv.kuchaj19.carleaseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarLeaseApiTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
